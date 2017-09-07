de06260fdb18204bd8b0e2ca0d2cee16be817d95
jellis720 <jellis720@gmail.com>
2017-09-07 16:44:56 -0400

    working

--- src/components/GetImageForm.js
+++ src/components/GetImageForm.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import GetImageButton from './GetImageButton';
-import ImageDisplay from './ImageDisplay';
+import ImageDisplay from './ImageDisplay.js';
+import GetImageButton from './GetImageButton.js';
 
-const API_KEY = "pwTpXANgJpqSBOrdi3LGYt8thLebtnP98YSIfYrW"
 
-export default class GetImageForm extends Component {
+const API_KEY = "pwTpXANgJpqSBOrdi3LGYt8thLebtnP98YSIfYrW";
+
+class GetImageForm extends Component {
   constructor(props){
     super(props);
 
@@ -17,7 +18,7 @@ export default class GetImageForm extends Component {
       rover: 'Curiosity',
       camera: 'FHAZ',
       images: [],
-      sol: 0,
+      sols: 0,
     }
   }
 
@@ -26,23 +27,30 @@ export default class GetImageForm extends Component {
   }
 
   handleCamera(event){
-    this.setState({rover: event.target.value})
+    this.setState({camera: event.target.value})
   }
 
   handleSol(event){
-    this.setState({rover: event.target.value})
+    this.setState({sol: event.target.value})
   }
 
   fetchRoverImage=()=>{
-    this.setState({camera: this.state.camera, rover: this.state.rover, sols: this.state.sols, images: []});
+    this.setState({
+      camera: this.state.camera,
+      rover: this.state.rover,
+      sols: this.state.sols,
+      images: []});
 
-    let camera = this.state.camera;
-    let rover = this.state.rover;
-    let solNum = this.state.sol;
-    let URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${solNum}&camera=${camera}&api_key=${API_KEY}`;
+    let camera1 = this.state.camera;
+    let rover1 = this.state.rover;
+    let solNum = this.state.sols;
 
-    fetch(URL).then(results=>results.json()).then(data=>{
-      this.setState({camera: this.state.camera, rover: this.state.rover, sol: this.state.sol, images: data.photos});
+    let imageURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover1}/photos?sol=${solNum}&camera=${camera1}&api_key=${API_KEY}`;
+
+    fetch(imageURL)
+    .then(results=>results.json())
+    .then(data => {
+      this.setState({camera: this.state.camera, rover: this.state.rover, sols: this.state.sols, images: data.photos});
     }).catch((error)=>{
       console.log('error', error)
     });
@@ -52,7 +60,7 @@ export default class GetImageForm extends Component {
     return (
       <div className="form_container">
         <div className="form-group">
-          <label htmlFor="roverLabel">
+          <label htmlFor="rover">
             Select Rover:
           </label>
           <select onChange={this.handleRover} id = "roverID" value= {this.state.value}>
@@ -60,19 +68,25 @@ export default class GetImageForm extends Component {
             <option value="Opportunity">Opportunity</option>
             <option value="Spririt">Spririt</option>
           </select>
-          <label htmlFor="camera">Camera View</label>
+          <label htmlFor="camera">Camera Type</label>
           <select onChange={this.handleCamera} id = "roverID" value= {this.state.value}>
-            <option value="fhaz">Front Hazard</option>
-            <option value="rhaz">Rear Hazard</option>
-            <option value="navcam">Navigation Camera</option>
+            <option value="fhaz">FHAZ (Front Hazard)</option>
+            <option value="rhaz">RHAZ (Rear Hazard)</option>
+            <option value="navcam">NAVCAM (Navigation Cam)</option>
           </select>
           <label htmlFor="sol">Martian Sol: 1000-2000</label>
           <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.value}/>
           <GetImageButton search={this.fetchRoverImage}/>
+
+          <div>
           <h1 className="roverName">{this.state.rover}</h1>
           <ImageDisplay photos={this.state.images}/>
+          </div>
+
         </div>
       </div>
     );
   }
 }
+
+export default GetImageForm;
--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const ImageDisplay=(props)=>{
   const pics = props.photos.map((photo)=>{
-    return (<img src={photo.img_src} key={photo.id} alt=" " className="img-responsive img"/>)
-  })
+    return (
+      <img src={photo.img_src} key={photo.id} alt=" " className="img-responsive image"/>
+    )
+  });
 
     return (
       <div className="pics_grid">
